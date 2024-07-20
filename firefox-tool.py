3c1127cbbc2e694a3af6d8bc062a787cefacc14e
Alan Bernstein <alanbernstein@fb.com>
2024-07-20 16:27:50 -0500

    Minor cleanup

--- firefox-tool.py
+++ firefox-tool.py
@@ -33,14 +33,14 @@ def main():
     p = FirefoxProfile()
 
     if sys.argv[1] == 'tabs':
-        p.print_session_markdown()
+        p.print_session_md()
     if sys.argv[1] == 'tabs-history':
         p.print_session_history()
     if sys.argv[1] in ['tabs-synced', 'synced-tabs', 'synced']:
         pattern = None
         if len(sys.argv) > 2:
             pattern = sys.argv[2]
-        p.print_synced_tabs_markdown(pattern)
+        p.print_synced_tabs_md(pattern)
     if sys.argv[1] == 'bookmarks':
         p.print_bookmarks()
     if sys.argv[1] == 'bookmarks-tree':
@@ -51,6 +51,8 @@ def main():
         print('not yet implemented')
     if sys.argv[1] in ['profile-path', 'profile']:
         print(p.get_profile_path())
+    if sys.argv[1] in ['render']:
+        p.render_search_page()
 
 def print_usage():
     print("""firefox-tool
@@ -72,8 +74,10 @@ class FirefoxProfile(object):
         self.profile_path = profile_path or self.get_profile_path()
         raw = mozlz4_to_text(self.get_session_file())
         self.session = json.loads(raw)
-        self.connect_bookmarks_db()
+        self.connect_places_db()
         self.connect_sync_db()
+        self.load_places_queries()
+        self.load_sync_queries()
 
     def get_profile_parent(self):
         if platform.system() == 'Darwin':
@@ -102,7 +106,7 @@ class FirefoxProfile(object):
         with open(fout, "w") as f:
             json.dump(self.session)
 
-    def connect_bookmarks_db(self):
+    def connect_places_db(self):
         db_file = '/tmp/places.sqlite'
         shutil.copy2(self.get_places_file(), db_file)
         self.places_connection = None
@@ -118,137 +122,97 @@ class FirefoxProfile(object):
         self.sync_connection = None
         self.sync_connection = sqlite3.connect(db_file)
 
-    def print_bookmarks(self):
-        # https://old.reddit.com/r/firefox/comments/4lr4e3/extracting_bookmarks_from_command_line_linux/
-        cursor = self.places_connection.cursor()
-        # cursor.execute('select * from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;')
-        cursor.execute('select moz_places.title, dateAdded, url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;')
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    def print_bookmarks_tree(self):
+    def load_places_queries(self):
         cursor = self.places_connection.cursor()
 
-        sql1 = "select id, url, title from moz_places"
-        cursor.execute(sql1)
+        field_list = ['id', 'url', 'title']
+        fields = ', '.join(field_list)
+        sql = 'select %s from moz_places' % fields
+        cursor.execute(sql)
         self.places_rows = cursor.fetchall()
-        self.places_rows_by_id = {r[0]: r for r in self.places_rows}
+        self.places_rows_by_id = {r[0]: {k: v for k, v in zip(field_list, r)} for r in self.places_rows}
 
-        sql2 = "select id, type, fk, parent, title from moz_bookmarks"
-        cursor.execute(sql2)
+        field_list = ['id', 'type', 'fk', 'parent', 'title']
+        fields = ', '.join(field_list)
+        sql = 'select %s from moz_bookmarks' % fields
+        cursor.execute(sql)
         self.bookmarks_rows = cursor.fetchall()
-        self.bookmarks_rows_by_id = {r[0]: r for r in self.bookmarks_rows}
+        self.bookmarks_rows_by_id = {r[0]: {k: v for k, v in zip(field_list, r)} for r in self.bookmarks_rows}
 
-        branch_node_rows = [r for r in self.bookmarks_rows if r[1] == 2]
+        # https://old.reddit.com/r/firefox/comments/4lr4e3/extracting_bookmarks_from_command_line_linux/
+        sql = 'select moz_places.title, dateAdded, url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;'
+        field_list = ['title', 'dateAdded', 'url']
+        cursor.execute(sql)
+        self.bookmarks_join_rows = cursor.fetchall()
+        self.bookmarks_join_rows_by_id = {r[0]: {k: v for k, v in zip(field_list, r)} for r in self.bookmarks_join_rows}
 
-        #for row in branch_node_rows[:30]:
-        #    print(row)
-
-        self.parent_to_children = defaultdict(list)
-        for row in self.bookmarks_rows:
-            self.parent_to_children[row[3]].append(row[0])
-
-        self.recurse_bookmarks_tree(1)
-
-    def recurse_bookmarks_tree(self, node_id, depth=0):
-        if node_id in self.bookmarks_rows_by_id:
-            row = self.bookmarks_rows_by_id[node_id]
-        else:
-            print('index error : %s' % node_id)
-            return
-
-        id_ = row[0]
-        typ = row[1]
-        fk = row[2]
-        parent = row[3]
-        title = row[4]
-        if fk in self.places_rows_by_id:
-            # leaf node with an associated url
-            url = self.places_rows_by_id[fk][1]
-            # print('%s(%s %s)%s (%s)' % (' ' * depth, id_, typ, title, url))
-            #print('[%s](%s)' % (title, url))  # markdown, nested sections
-            print('%s- [%s](%s)' % ('  ' * depth, title, url)) # markdown, nested list
-        else:
-            # print('%s(%s %s)%s' % (' ' * depth, id_, typ, title))
-            # print('%s %s' % ('#' * depth, title)) # markdown, nested sections
-            print('%s- %s' % ('  ' * depth, title)) # markdown, nested list
-            if node_id in self.parent_to_children:
-                for c in self.parent_to_children[node_id]:
-                    self.recurse_bookmarks_tree(c, depth+1)
-
-
-    def inspect_bookmarks_tree(self):
-        cursor = self.places_connection.cursor()
-
-        sql2 = "select id, type, fk, parent, title from moz_bookmarks"
-        cursor.execute(sql2)
-        rows = cursor.fetchall()
-
-        parent_to_children = defaultdict(list)
-        for row in rows:
-            parent_to_children[row[3]].append(row[0])
-
-        parents = parent_to_children.keys()
-        for branch_node_id in sorted(parents)[:30]:
-            fk = rows[branch_node_id][2]
-            parent = rows[branch_node_id][3]
-            title = rows[branch_node_id][4]
-            print('%d: fk: %s, parent: %s, title: %s' % (branch_node_id, fk, parent, title))
-            print('  children: %s' % (parent_to_children[branch_node_id]))
-
-
-        db()
-
-        # cursor.execute('select * from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk;')
-        # SELECT moz_places.id, moz_places.url, moz_places.title, moz_bookmarks.parent FROM moz_places LEFT OUTER JOIN moz_bookmarks N moz_places.id = moz_bookmarks.fk WHERE moz_bookmarks.parent = N
-        sql = ('SELECT moz_places.id, moz_places.url, moz_places.title, moz_bookmarks.parent'
-               'FROM moz_places'
-               'LEFT OUTER JOIN moz_bookmarks'
-               'N moz_places.id = moz_bookmarks.fk'
-               'WHERE moz_bookmarks.parent = N')
-
-        sql1 = ("select moz_places.title, dateAdded, url"
-                "from moz_bookmarks, moz_places"
-                "where moz_places.id=moz_bookmarks.fk")
-        sql1 = "select moz_places.id, fk, parent, moz_places.title, dateAdded, url from moz_bookmarks, moz_places where moz_places.id=moz_bookmarks.fk"
-
-        cursor.execute(sql1)
-        rows = cursor.fetchall()
-
-        for row in rows:
-            print(row)
-
-        
-
-    def print_bookmarks_tree_html(self):
-        # print tree structure of bookmark folders to an html file
-        # each level is show/hide-able
-        # search box that searches all urls and titles, and hides non-match branches
-        pass
-
-    def print_synced_tabs_markdown(self, device_name_pattern):
+    def load_sync_queries(self):
         cursor = self.sync_connection.cursor()
 
         # get last sync time
         cursor.execute("select * from moz_meta where key='last_sync_time'")
         rows = cursor.fetchall()
-        last_sync_time_ts = rows[0][1]
-        last_sync_time = datetime.datetime.fromtimestamp(last_sync_time_ts//1000)
+        self.last_sync_time_ts = rows[0][1]
+        last_sync_time = datetime.datetime.fromtimestamp(self.last_sync_time_ts//1000)
         last_sync_time_str = datetime.datetime.strftime(last_sync_time, time_fmt)
         last_sync_delta = now - last_sync_time
-        print('last sync: %s (%s ago)' % (last_sync_time_str, last_sync_delta))
+        self.last_sync_str = 'last sync: %s (%s ago)' % (last_sync_time_str, last_sync_delta)
 
         # get device metadata (not really needed)
         cursor.execute("select * from moz_meta where key='remote_clients'")
-        rows = cursor.fetchall()
-        remote_devices = json.loads(rows[0][1])
-        device_id_name_map = {k: v['device_name'] for k, v in remote_devices.items()}
+        self.meta_rows = cursor.fetchall()
+        remote_devices = json.loads(self.meta_rows[0][1])
+        self.device_id_name_map = {k: v['device_name'] for k, v in remote_devices.items()}
 
-        # get synced tabs
-        cursor.execute('select * from tabs')
-        rows = cursor.fetchall()
-        for row in rows:
+        # get tab data
+        cursor.execute('select * from tabs;')
+        self.tab_rows = cursor.fetchall()
+
+    def print_bookmarks(self):
+        for row in self.bookmarks_join_rows:
+            print(row)
+
+    def print_bookmarks_folders(self):
+        branch_node_rows = [r for r in self.bookmarks_rows if r[1] == 2]
+        for row in branch_node_rows[:30]:
+            print(row)
+
+    def print_bookmarks_tree(self):
+        ROOT_NODE_ID = 1
+        self.parent_to_children = defaultdict(list)
+        for id, row in self.bookmarks_rows_by_id.items():
+            self.parent_to_children[row['parent']].append(row['id'])
+        
+        self.recurse_bookmarks_tree_mdlist(ROOT_NODE_ID)
+        #self.recurse_bookmarks_tree_mddoc(ROOT_NODE_ID)
+
+    def recurse_bookmarks_tree_mdlist(self, node_id, depth=0):
+        row = self.bookmarks_rows_by_id[node_id]
+
+        if row['fk'] in self.places_rows_by_id:
+            url = self.places_rows_by_id[row['fk']]['url']
+            print('%s- [%s](%s)' % ('  ' * depth, row['title'], url))
+        else:
+            print('%s- %s' % ('  ' * depth, row['title']))
+            if node_id in self.parent_to_children:
+                for c in self.parent_to_children[node_id]:
+                    self.recurse_bookmarks_tree_mdlist(c, depth+1)
+    
+    def recurse_bookmarks_tree_mddoc(self, node_id, depth=0):
+        row = self.bookmarks_rows_by_id[node_id]
+
+        if row['fk'] in self.places_rows_by_id:
+            url = self.places_rows_by_id[row['fk']]['url']
+            print('[%s](%s)' % (row['title'], url))
+        else:
+            print('%s %s' % ('#' * depth, row['title']))
+            if node_id in self.parent_to_children:
+                for c in self.parent_to_children[node_id]:
+                    self.recurse_bookmarks_tree_mddoc(c, depth+1)
+        
+    def print_synced_tabs_md(self, device_name_pattern):
+        print(self.last_sync_str)
+        for row in self.tab_rows:
             id, record, last_modified = row
             data = json.loads(record)
             device_name = data['clientName']
@@ -264,7 +228,8 @@ class FirefoxProfile(object):
             for tab in tabs:
                 urls = tab['urlHistory']
                 if len(urls) > 1:
-                    db()
+                    pass
+                    # db()
                 url = urls[0]
 
                 last_used_ts = tab['lastUsed']
@@ -280,7 +245,7 @@ class FirefoxProfile(object):
                 elif overflow_mode == 'wrap':
                     print(line)
 
-    def print_session_markdown(self):
+    def print_session_md(self):
         for wnum, w in enumerate(self.session['windows']):
             print('')
             print('## window %s (%s tabs)' % (wnum, len(w['tabs'])))
@@ -302,6 +267,20 @@ class FirefoxProfile(object):
                         print('  %d: %s %s' % (tnum, url, e['title']))
                     else: 
                         print('      %s%s %s' % (en_num*' ', url, e['title']))
+    
+    def render_search_page(self):
+        # print tree structure of bookmark folders to an html file (via md)
+        # each level is show/hide-able
+        # search box that searches all urls and titles, and hides non-match branches
+
+        # write tabs.md, then pandoc -> html (or just write html)
+        # write bookmarks.html (tree structured jinja won't work)
+        # use ff-search-template.html jinja template to combine
+
+        # print last sync time
+        # print render time
+        self.print_synced_tabs_md('tabs.html')
+        self.print
 
     def inspect_session(self):
         for k in self.session.keys():
